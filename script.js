b0af141c35613d35cdfaea754d19b1700efe54ff
AmiraBelkis <ia_feliachi@esi.dz>
2021-08-31 21:37:39 +0100

    Structuring the code

--- script.js
+++ script.js
@@ -3,26 +3,57 @@ for (let i = 0; i < selectors.length - 2; i++) {
     selectors[i].addEventListener('mouseenter', () => selectors[i].children[1].style = 'display:block');
     selectors[i].addEventListener('mouseleave', () => selectors[i].childNodes[3].style = 'display:none');
 }
-let inputs = document.querySelectorAll(".input");
+let val = document.getElementsByClassName('val');
 let x, y, Blur, spread, opacity, boxColor, shadowColor;
-/*--------------*/
-function start() {
-    x = document.getElementById("x").value;
-    y = document.getElementById("y").value;
-    Blur = document.getElementById("blur").value;
-    spread = document.getElementById("spread").value;
-    opacity = document.getElementById("opacity").value;
-    boxColor = document.getElementById("boxColorPicker").value;
-    shadowColor = document.getElementById("shadowColorPicker").value;
-    //-------------------------------------------
+/*-------------X--------------*/
+function X() {
     document.documentElement.style.setProperty("--x", x + "px");
+    x = document.getElementById("x").value;
+    val[0].innerHTML = x;
+
+}
+/*-------------Y--------------*/
+function Y() {
+    y = document.getElementById("y").value;
     document.documentElement.style.setProperty("--y", y + "px");
+    val[1].innerHTML = y;
+}
+/*-------------blur--------------*/
+function shadowBlur() {
+    Blur = document.getElementById("blur").value;
     document.documentElement.style.setProperty("--blur", Blur + "px");
+    val[3].innerHTML = Blur;
+
+}
+
+/*-------------spread--------------*/
+function Spread() {
+    spread = document.getElementById("spread").value;
     document.documentElement.style.setProperty("--spread", spread + "px");
+    val[2].innerHTML = spread;
+
+}
+
+/*-------------opacity--------------*/
+function Opacity() {
+    val[4].innerHTML = opacity;
+    opacity = document.getElementById("opacity").value;
     document.documentElement.style.setProperty("--opacity", opacity);
+
+}
+
+/*-------------box color--------------*/
+function BoxColor() {
+    boxColor = document.getElementById("boxColorPicker").value;
     document.documentElement.style.setProperty("--boxColor", boxColor);
+
+}
+
+/*-------------shadow color--------------*/
+function ShadowColor() {
+    shadowColor = document.getElementById("shadowColorPicker").value;
     document.documentElement.style.setProperty("--shadowColor", shadowColor);
-    //-------------------------------------------
+
 }
 /*--------*/
 function copy() {
